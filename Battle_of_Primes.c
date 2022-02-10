03fbbcc36b49238afb5fbac2efe220705deaff7b
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-10 18:34:27 +0530

    //kattappa and bahubali #include<stdio.h> int main() { 	int a,b; 	scanf("%d%d",&a,&b); 	int c,count=0,i,j,kills; 	for (i=1; i>0; i++) 	{ 		kills=a+b+i; 		count=0; 		for (j=1; j<=kills; j++) 		{ 			if (kills%j==0) 			{ 				count+=1; 			} 		} 		if (count<=2) 		{ 			printf("%d",i); 			break; 		} 	} 	return 0; }

---
+++ Battle_of_Primes.c
@@ -0,0 +1,26 @@
+//kattappa and bahubali
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int c,count=0,i,j,kills;
+	for (i=1; i>0; i++)
+	{
+		kills=a+b+i;
+		count=0;
+		for (j=1; j<=kills; j++)
+		{
+			if (kills%j==0)
+			{
+				count+=1;
+			}
+		}
+		if (count<=2)
+		{
+			printf("%d",i);
+			break;
+		}
+	}
+	return 0;
+}
