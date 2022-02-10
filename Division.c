b9a9d4eed136f4acec29364cedc0ac9ab1170b53
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-10 17:28:58 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int r;     if (a,b>=1 && a,b<=10000)     {         r=a/b;         printf("%d",r);     }     return 0; }

---
+++ Division.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int r;
+    if (a,b>=1 && a,b<=10000)
+    {
+        r=a/b;
+        printf("%d",r);
+    }
+    return 0;
+}
