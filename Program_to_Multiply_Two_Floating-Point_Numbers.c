4072cd940924711b37c0c5b37bb706ea4f2d1327
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-10 18:17:23 +0530

    #include<stdio.h> int main() {     float a,b;     scanf("%f%f",&a,&b);     double pro;     if (a,b<=1 && a,b<=10000)     {         pro=a*b;     }     printf("%.2f",pro);     return 0; }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    scanf("%f%f",&a,&b);
+    double pro;
+    if (a,b<=1 && a,b<=10000)
+    {
+        pro=a*b;
+    }
+    printf("%.2f",pro);
+    return 0;
+}
