83350ce9bf42335c14cb3f511d3b18be8e8c45e1
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-10 17:26:22 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int r;     if (a,b>=1)     {         if (a,b<=10000)         {             r=a*b;         }     }     printf("%d",r); }

---
+++ Multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int r;
+    if (a,b>=1)
+    {
+        if (a,b<=10000)
+        {
+            r=a*b;
+        }
+    }
+    printf("%d",r);
+}
