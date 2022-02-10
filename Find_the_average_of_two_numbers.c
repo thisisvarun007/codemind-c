4a2d6b5caaf115e80fcc77b870f2baf5aa8df1c0
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-10 17:25:45 +0530

    #include<stdio.h> int main() {     double N,M;     double avg;     scanf("%lf%lf",&N,&M);     if (N,M>=1 && N,M<=1000)     {         avg=(N+M)/2;     }     printf("%.4lf",avg); }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    double N,M;
+    double avg;
+    scanf("%lf%lf",&N,&M);
+    if (N,M>=1 && N,M<=1000)
+    {
+        avg=(N+M)/2;
+    }
+    printf("%.4lf",avg);
+}
