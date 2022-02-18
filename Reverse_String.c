126e926c5579d5203007d6c3efd16e5489c6ac2d
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-02-18 09:20:24 +0530

    #include<stdio.h> int main() {     char str[100],s[100];     int i,c=0;     scanf("%[^ ]s",str);     for (i=0; str[i]!=NULL; i++)     {         c++;     }     for (i=c-1; i>=0; i--)     {         printf("%c",str[i]);     }     return 0; }

---
+++ Reverse_String.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[100],s[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for (i=0; str[i]!=NULL; i++)
+    {
+        c++;
+    }
+    for (i=c-1; i>=0; i--)
+    {
+        printf("%c",str[i]);
+    }
+    return 0;
+}
