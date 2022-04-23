b78ce918f351077be5c406539dd5d5fd8579568b
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-04-23 11:16:37 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int i,j;     for (i=0; i<a; i++)     {         for (j=0; j<a; j++)         {             if ((i==j) || (j==0) || (j==a-1))             {                 printf("* ");             }             else             {                 printf("  ");             }         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,19 +3,22 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    char arr[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i,j;
-    for (i=a-1; i>=0; i--)
+    for (i=0; i<a; i++)
     {
-        for (j=1; j<=i+1; j++)
+        for (j=0; j<a; j++)
         {
-            printf("%c ",arr[i]);
-            if (j==i+1)
+            if ((i==j) || (j==0) || (j==a-1))
             {
-                printf("
-");
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
             }
         }
+        printf("
+");
     }
     return 0;
 }
