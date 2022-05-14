f193d10ae7c9fb17ea069318713a0a5b5c5719c1
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-05-14 18:44:32 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[1000];     scanf("%[^ ]s",str);     int i,j,l=strlen(str);     int i1=l-1,i2=l-1;     for (i=l-1; i>=0; i--)     {         if (str[i]==32 || i==0)         {             if (i==0)             {                 for (j=i; j<=i2; j++)                 {                     printf("%c",str[j]);                 }             }             else             {                 for (j=i+1; j<=i2; j++)                 {                     printf("%c",str[j]);                 }             }             i2=i-1;             if (i>0)             {                 printf(" ");             }         }     }     return 0; }

--- Reverse_String.c
+++ Reverse_String.c
@@ -1,17 +1,36 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    char str[100],s[100];
-    int i,c=0;
+    char str[1000];
     scanf("%[^
 ]s",str);
-    for (i=0; str[i]!=NULL; i++)
+    int i,j,l=strlen(str);
+    int i1=l-1,i2=l-1;
+    for (i=l-1; i>=0; i--)
     {
-        c++;
-    }
-    for (i=c-1; i>=0; i--)
-    {
-        printf("%c",str[i]);
+        if (str[i]==32 || i==0)
+        {
+            if (i==0)
+            {
+                for (j=i; j<=i2; j++)
+                {
+                    printf("%c",str[j]);
+                }
+            }
+            else
+            {
+                for (j=i+1; j<=i2; j++)
+                {
+                    printf("%c",str[j]);
+                }
+            }
+            i2=i-1;
+            if (i>0)
+            {
+                printf(" ");
+            }
+        }
     }
     return 0;
 }
